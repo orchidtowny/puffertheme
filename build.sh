303fbdb8002b1061cc97e70b6a3c2a5c1fd94910
ihateblueb <ihateblueb@proton.me>
2025-08-25 16:16:34 -0400

    Up Date

--- build.sh
+++ build.sh
@@ -3,25 +3,36 @@ rm SamuelTheme.tar > /dev/null 2>&1
 
 echo "Making directories..."
 mkdir build-tmp
-mkdir build-tmp/components
-mkdir build-tmp/fonts
-mkdir build-tmp/img
 
-echo "Copying main styles and manifest..."
-cp fonts.css build-tmp/fonts.css
-cp theme.css build-tmp/theme.css
-cp manifest.json build-tmp/manifest.json
+echo "Compiling custom CSS..."
+touch build-tmp/custom.css
 
-echo "Copying other styles, images, and fonts..."
-cp -r components/ build-tmp/components/
-cp -r fonts/ build-tmp/fonts/
-cp -r img/ build-tmp/img/
+echo " e "
+echo " e "
+echo " e "
+
+ls
+mkdir build-tmp/css-tmp
+find custom -name 'custom/*.css' -type f -exec cp {} build-tmp/css-tmp/ \;
+find custom -name 'custom/componments/*.css' -type f -exec cp {} build-tmp/css-tmp/ \;
+find custom -name 'custom/fonts/*.css' -type f -exec cp {} build-tmp/css-tmp/ \;
+find custom -name 'custom/img/*.png' -type f -exec cp {} build-tmp/css-tmp/ \;
+
+echo " e "
+echo " e "
+echo " e "
 
 cd build-tmp
 
+echo "Copying manifest..."
+cp -r ../manifest.json manifest.json
+
 echo "Archiving..."
-tar cf ../SamuelTheme.tar . 
+tar cf ../SamuelTheme . 
 
 cd - > /dev/null 2>&1
-rm -R build-tmp
+#rm -R build-tmp
 echo "Cleaning build temp files..."
+
+echo "Renaming archive..."
+mv SamuelTheme SamuelTheme.tar
